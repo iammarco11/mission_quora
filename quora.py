8af21b950f116853cf02a94a2a37186f13960f27
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-08-18 11:22:10 +0530

    Quora: Add input option for URL

--- quora.py
+++ quora.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://www.quora.com/What-are-the-pro-tips-for-GSOC"
+url = input("Input the Discussion URL of Quora:")
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -15,6 +15,6 @@ f.close()
 
 result = soup.find_all("script",{"type": "application/ld+json"})
 
-g = open("quora.json","w")
+g = open("quora.html","w")
 g.write(str(result))
 g.close()
