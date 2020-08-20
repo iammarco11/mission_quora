6574e3ceb715dbc4aff08120fb74989698f988b9
abhijitramesh <abhijitramesh2k@gmail.com>
2020-08-20 10:48:35 +0530

    Feat : remove html tags

--- quora.py
+++ quora.py
@@ -3,6 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+import re
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
 url = input("Input the Discussion URL of Quora:")
 
 page = requests.get(url)
@@ -13,7 +20,9 @@ f = open("index.html", "w")
 f.write(indexhtml)
 f.close()
 
-result = soup.find_all("script",{"type": "application/ld+json"})
+result = soup("script",{"type": "application/ld+json"})
+result = cleanhtml(str(result))
+print(result)
 
 g = open("quora.html","w")
 g.write(str(result))
