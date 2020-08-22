4dfa825714747a72d0ca5b6e9676b271665cb53d
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-08-22 05:30:29 +0530

    Quora: Feat: Get details of Authors and Answers

--- quora.py
+++ quora.py
@@ -6,9 +6,25 @@ import json
 import re
 
 def cleanhtml(raw_html):
-  cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
+	cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+	cleantext = re.sub(cleanr, '', raw_html)
+	return cleantext
+
+
+def interact_json(json_obj):
+	with open("quora.json", "w+") as f:
+		f.write(json.dumps(json_obj, indent=4, sort_keys=True))
+
+	with open("quora.json") as json_file:
+		data = json.load(json_file)
+
+	answers = data[0]['mainEntity']['suggestedAnswer']
+	
+	for i in range(0, len(answers)):
+		print(answers[i]['author']['url'])
+		print("\n")
+		print(answers[i]['text'])
+		print("\n")
 
 url = input("Input the Discussion URL of Quora:")
 
@@ -22,8 +38,7 @@ f.close()
 
 result = soup("script",{"type": "application/ld+json"})
 result = cleanhtml(str(result))
-print(result)
 
-g = open("quora.json","w")
-g.write(str(result))
-g.close()
+json_object = json.loads(str(result))
+
+interact_json(json_object)
